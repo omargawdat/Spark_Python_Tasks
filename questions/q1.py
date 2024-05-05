03027bf92405cdaea4be29a7b19ebe1908ec5ca1
omargawdat <omargawdaat@gmail.com>
2024-05-05 12:57:11 +0300

    refactor: enhance codes and make the them clener

--- questions/q1.py
+++ questions/q1.py
@@ -6,12 +6,27 @@ class Question1(DataProcessingTask):
         super().__init__(question_number=1)
 
     def process_data_with_loops(self, rdd):
+        # Collecting and processing data on the driver to simulate a loop
         page_size_pairs = rdd.flatMap(lambda line: [
             ('key', int(line.split(" ")[-1])) if line.split(" ")[-1] != 'null' and len(line.split(" ")) > 1 else (
-                'key', 0)])
-        grouped = page_size_pairs.groupByKey().mapValues(list)
-        results = grouped.map(lambda x: (min(x[1]), max(x[1]), sum(x[1]) / len(x[1])) if x[1] else (0, 0, 0.0))
-        min_size, max_size, avg_size = results.collect()[0] if results.collect() else (0, 0, 0.0)
+                'key', 0)]).collect()
+
+        # Creating a dictionary to simulate grouping by key
+        grouped_sizes = {}
+        for key, size in page_size_pairs:
+            if key not in grouped_sizes:
+                grouped_sizes[key] = []
+            grouped_sizes[key].append(size)
+
+        # Calculating min, max, and average sizes from the collected sizes
+        if grouped_sizes['key']:
+            sizes = grouped_sizes['key']
+            min_size = min(sizes)
+            max_size = max(sizes)
+            avg_size = sum(sizes) / len(sizes)
+        else:
+            min_size = max_size = avg_size = 0
+
         output = f"Minimum page size: {min_size}\nMaximum page size: {max_size}\nAverage page size: {avg_size:.2f}"
         return output
 
--- questions/q3.py
+++ questions/q3.py
@@ -7,20 +7,19 @@ class Question3(DataProcessingTask):
         super().__init__(question_number=3)
 
     def process_data_with_loops(self, rdd):
-        # This method collects all titles, splits and normalizes them, simulating a loop over the entire dataset
         titles = rdd.map(lambda x: x.split(" ")[1]).collect()
         unique_terms = set()
+
         for title in titles:
-            normalized_terms =  re.sub(r'[^a-zA-Z0-9_]', '', title.lower()).split("_")
+            normalized_terms = re.sub(r'[^a-zA-Z0-9_]', '', title.lower()).split("_")
             unique_terms.update(normalized_terms)
+
         return f"Number of unique terms in page titles: {len(unique_terms)}"
 
     def process_data_with_mapreduce(self, rdd):
-        # Using Spark's powerful map-reduce capabilities
         unique_terms_count = rdd \
             .map(lambda x: x.split(" ")[1]) \
             .flatMap(lambda title: re.sub(r'[^a-zA-Z0-9_]', '', title.lower()).split("_")) \
             .distinct() \
             .count()
         return f"Number of unique terms in page titles: {unique_terms_count}"
-
